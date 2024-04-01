694da64dd393681dd29a3bfa2ff85c1ba450288f
Rytopium <156077547+Rytopium@users.noreply.github.com>
2024-04-01 11:52:26 +1100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,13 @@
-npm install firebase
-// firebaseConfig.js
 const admin = require('firebase-admin');
-
 const serviceAccount = require('./path/to/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'YOUR_DATABASE_URL'
+  databaseURL: process.env.FIREBASE_DATABASE_URL // Use environment variable for database URL
 });
 
 const db = admin.firestore();
 
 module.exports = { db };
+
 
